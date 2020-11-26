80225092f9db34287d9022d2ef947cd62d8a94dc
yunteng <yunteng@xgd.com>
2020-11-26 15:34:41 +0800

    go go go

---
+++ src/1281. Subtract the Product and Sum of Digits of an Integer/main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	a := 234
+	subtractProductAndSum(a)
+
+}
+
+func subtractProductAndSum(n int) int {
+	j := 1
+	h := 0
+	for n > 0 {
+		y := n % 10
+		n = n / 10
+		j = j * y
+		h = h + y
+	}
+	return j - h
+}
