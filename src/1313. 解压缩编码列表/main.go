64aa428eb2846323a3ed273b94f28a4a76086588
yunteng <yunteng@xgd.com>
2020-12-01 17:13:51 +0800

    go go go

--- src/1313. 解压缩编码列表/main.go
+++ src/1313. 解压缩编码列表/main.go
@@ -1,20 +1,20 @@
-package mainfunc decompressRLElist(nums []int) []int {
-var result []int
-for i, v := range nums {
-if i%2 != 0 {
-for j := 0; j < nums[i - 1]; j++ {
-result = append(result, v)
-}
-}
-}
-return result
-}
+package main
 
 import "fmt"
+
+func decompressRLElist(nums []int) []int {
+	var result []int
+	for i, v := range nums {
+		if i%2 != 0 {
+			for j := 0; j < nums[i-1]; j++ {
+				result = append(result, v)
+			}
+		}
+	}
+	return result
+}
 
 func main() {
 	nums := []int{1, 3, 2, 3}
 	fmt.Println(decompressRLElist(nums))
 }
-
-
