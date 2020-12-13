38a3f1723900ebd5ff6bfd95309e7473e66d2332
云腾 <yunteng@xgd.com>
2020-12-13 10:32:16 +0800

    add

--- src/1221. 分割平衡字符串/main.go
+++ src/1221. 分割平衡字符串/main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	fmt.Println(balancedStringSplit("RLRRLLRLRL"))
+}
+
+func balancedStringSplit(s string) int {
+	res := 1
+	for i := range s {
+		if i > 0 && s[i] != s[i-1] {
+			res++
+		}
+	}
+	fmt.Println("hello")
+	return res
 }
