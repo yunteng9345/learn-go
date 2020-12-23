b056f8458931f9f457723e2c0bfadbc6d1bbfc02
yunteng <yunteng@xgd.com>
2020-12-23 17:01:22 +0800

    go go go

---
+++ src/1351. 统计有序矩阵中的负数/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(countNegatives([][]int{{4, 3, 2, -1}, {3, 2, 1, -1}, {1, 1, -1, -2}, {-1, -1, -2, -3}}))
+}
+func countNegatives(grid [][]int) int {
+	res := 0
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if grid[i][j] < 0 {
+				res++
+			}
+		}
+	}
+	return res
+}
