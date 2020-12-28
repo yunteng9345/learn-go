ed69400ece8d3f57025800c1bb5e48c24c456612
yunteng <yunteng@xgd.com>
2020-12-28 15:08:24 +0800

    go go go

--- src/剑指 Offer 06. 从尾到头打印链表/main.go
+++ src/剑指 Offer 06. 从尾到头打印链表/main.go
@@ -1,7 +1,13 @@
 package main
 
-func main() {
+import (
+	"strconv"
+	"strings"
+)
 
+func main() {
+	head := CreateTestData("[1,2,3,4,5]")
+	reversePrint(head)
 }
 
 /**
@@ -17,11 +23,39 @@ type ListNode struct {
 }
 
 func reversePrint(head *ListNode) []int {
+	if head == nil {
+		return nil
+	}
 	var res []int
-	i := 0
-	if head != nil {
-		res[i] = head.Val
+	for head != nil {
+		res = append(res, head.Val)
+		head = head.Next
+	}
+	for i, j := 0, len(res)-1; i < j; {
+		res[i], res[j] = res[j], res[i]
 		i++
+		j--
 	}
 	return res
+}
+
+func CreateTestData(data string) *ListNode {
+	if data == "[]" {
+		return nil
+	}
+	data = string([]rune(data)[1 : len(data)-1])
+	res := strings.Split(data, ",")
+	length := len(res)
+	listNode := make([]ListNode, length)
+	headVal, err := strconv.Atoi(res[0])
+	if err != nil {
+		panic(err)
+	}
+	listNode[0] = ListNode{headVal, nil}
+	for i := 1; i < length; i++ {
+		headVal, _ = strconv.Atoi(res[i])
+		listNode[i] = ListNode{headVal, nil}
+		listNode[i-1].Next = &listNode[i]
+	}
+	return &listNode[0]
 }
