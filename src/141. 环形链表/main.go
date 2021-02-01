ae9538d18b2c788e4099b279c63d49f4c52b78bc
yunteng <yunteng@xgd.com>
2021-02-01 15:03:47 +0800

    go go go

---
+++ src/141. 环形链表/main.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+func main() {
+	dict := map[string]string{}
+	dict["foo"] = "f"
+	if val, ok := dict["foo"]; ok {
+		//do something here
+		fmt.Println(val)
+		fmt.Println(ok)
+	}
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// hash table
+func hasCycle1(head *ListNode) bool {
+	seen := map[*ListNode]struct{}{}
+	for head != nil {
+		if _, ok := seen[head]; ok {
+			return true
+		}
+		seen[head] = struct{}{}
+		head = head.Next
+	}
+	return false
+}
+
+// double pointer
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+	slow, fast := head, head.Next
+	for slow != fast {
+		if fast == nil || fast.Next == nil {
+			return false
+		}
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return true
+}
