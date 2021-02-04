f64cf6de9fbb3b982739009694b7df0bd15edd7d
yunteng <yunteng@xgd.com>
2021-02-04 14:09:40 +0800

    go go go

---
+++ src/21. 合并两个有序链表/main.go
@@ -0,0 +1,60 @@
+package main
+
+func main() {
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 输入：l1 = [1,2,4], l2 = [1,3,4]
+// 输出：[1,1,2,3,4,4]
+
+// 递归
+func mergeTwoLists1(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	} else {
+		l2.Next = mergeTwoLists(l1, l2.Next)
+		return l2
+	}
+}
+
+// 哨兵
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+	prev := dummy
+	for l1 != nil && l2 != nil {
+		if l1.Val > l2.Val {
+			prev.Next = l2
+			l2 = l2.Next
+		} else {
+			prev.Next = l1
+			l1 = l1.Next
+		}
+		prev = prev.Next
+	}
+	if l1 != nil {
+		prev.Next = l1
+	}
+	if l2 != nil {
+		prev.Next = l2
+	}
+	return dummy.Next
+}
